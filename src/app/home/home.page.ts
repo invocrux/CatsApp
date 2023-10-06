687df4ba40de3b94826f12cfe6323df7c4adab83
ing Roberto De La Cruz <invocrux17@outlook.com>
2023-10-06 16:06:23 -0500

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CatAppService } from './service/cat-app.service';
 import { CatAPI } from './interface/interface';
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,32 @@ import { CatAPI } from './interface/interface';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-  data: CatAPI[] = []
+  cats: CatAPI[] = []
+  imagesObservables: { [key: string]: Observable<any> } = {}
+  images: { [x: string]: any; } = {};
   constructor(private readonly service: CatAppService) {}
   ngOnInit(): void {
-    this.service.getDataApi().subscribe(e => this.data = e )
+    this.getCats()
+  }
+
+  getCats(){
+
+    this.service.getDataApi().subscribe(e => {
+      this.cats = e
+      this.fillImages()
+    })
+  }   
+  fillImages() {
+    this.cats.forEach((cat) => {
+      if(!cat.reference_image_id) return
+      this.imagesObservables[cat.reference_image_id] = this.service.getImage(cat.reference_image_id)
+    })
+    forkJoin(this.imagesObservables).subscribe((resp) => this.images = resp)
+  }
+
+  getImages(idImage: string | undefined){
+    if(!idImage) return '../../assets/cat.png.jpg'
+    return this.images[idImage]
   }
 
 }
--- src/app/home/service/cat-app.service.ts
+++ src/app/home/service/cat-app.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CatAPI } from '../interface/interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatAppService {
-
+  headers = {
+    'x-api-key': 'bda53789-d59e-46cd-9bc4-2936630fde39'
+  }
   constructor(private http: HttpClient) { }
 
   getDataApi(){
-    const headers = {
-      'x-api-key': 'bda53789-d59e-46cd-9bc4-2936630fde39'
-    }
+    
     const consulta = 'https://api.thecatapi.com/v1/breeds'
-    return this.http.get<CatAPI[]>(consulta, {headers})
+    return this.http.get<CatAPI[]>(consulta, {headers: this.headers})
+  }
+
+  getImage(idImage: string){
+    const consulta = `https://api.thecatapi.com/v1/images/${idImage}` 
+    return this.http.get<any>(consulta, {headers: this.headers}).pipe(map(item => item.url))
   }
 }
